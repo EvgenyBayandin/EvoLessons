567eef23d2e8dcbd85ef4ba06be1fc6f7d760096
Evgeny Bayandin <5087081@gmail.com>
2024-05-13 19:50:24 +0300

    Update files

--- src/main/java/ru/webdev/oop/Learner.java
+++ src/main/java/ru/webdev/oop/Learner.java
@@ -1,13 +1,5 @@
 package ru.webdev.oop;
 
-public class Learner {
-
-    private String name;
-    private int age;
-    private int group;
-
-    public void learn(){
-        System.out.println("Learning...");
-    }
-
+public interface Learner {
+    void learn();
 }
--- src/main/java/ru/webdev/oop/Student.java
+++ src/main/java/ru/webdev/oop/Student.java
@@ -1,7 +1,16 @@
 package ru.webdev.oop;
 
-public class Student {
+import java.util.stream.IntStream;
+
+public class Student implements Learner {
     private String name;
     private int age;
     private int group;
+
+    @Override
+    public void learn() {
+        System.out.println("Я учусь. .zZ");
+        IntStream.range(0, 1000000).forEach(i -> Math.log(i));
+        System.out.println("Я закончил учиться");
+    }
 }
