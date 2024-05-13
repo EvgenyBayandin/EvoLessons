5b9ef1f1f74b345d5c93e479bf6c5a0f43b16d29
Evgeny Bayandin <5087081@gmail.com>
2024-05-13 19:30:15 +0300

    Added new class Learner

---
+++ src/main/java/ru/webdev/oop/Learner.java
@@ -0,0 +1,13 @@
+package ru.webdev.oop;
+
+public class Learner {
+
+    private String name;
+    private int age;
+    private int group;
+
+    public void learn(){
+        System.out.println("Learning...");
+    }
+
+}
